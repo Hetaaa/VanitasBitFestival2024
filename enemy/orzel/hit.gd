4516a4ec1aa849fff33b524177fa0133a140d523
Daniel Szota <daniel.szota@explitia.com>
2024-11-30 18:15:52 +0100

    dziala

---
+++ enemy/orzel/hit.gd
@@ -0,0 +1,19 @@
+extends State
+
+var myowner 
+@onready var state_machine = get_parent()
+var speed_mock = 0.0
+func Enter():
+	myowner = state_machine.get_parent()
+	speed_mock = myowner.SPEED
+	myowner.SPEED = 0.0
+	
+	await get_tree().create_timer(0.3).timeout
+	myowner.attacking()
+	await get_tree().create_timer(0.6).timeout
+	myowner.SPEED = speed_mock
+	if state_machine._get_state_name() != "Dead":
+		Transitioned.emit("Attack")
+func PhysicsUpdate(delta):
+	myowner.SPEED = 0.0
+	myowner.velocity = Vector2(0,0)
--- enemy/orzel/orzel.gd
+++ enemy/orzel/orzel.gd
@@ -4,7 +4,13 @@ extends CharacterBody2D
 @export var SPEED = 10.0
 var health = 100
 var hold = false
-var knockback_force = 400
+var knockback_force = 100
+@onready var col = $col
+@onready var state_machine = $State_Machine
+@onready var hit_area = $Contents/hit_area
+@onready var contents = $Contents
+var offset_treshold =60;
+var current_offset = 0;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -15,25 +21,20 @@ func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	var direction = null;
-	if !hold:
-		if global_position.x - player.global_position.x >= 0:
-			direction = -1
-		else:
-			direction = 1
-		print(direction)
-		if direction:
-			velocity.x = direction*SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
 func get_hit(dmg, dir):
-	print("ALLLLLAAAAA")
+	print(dmg)
 	health -= dmg
+	if health <= 0:
+		state_machine.change_state("Dead")
+	else:
+		state_machine.change_state("Stun")
 	
-	var direction = global_position - dir
-	direction = direction.normalized()  # Normalizowanie wektora, aby zachować stałą siłę odpychania
-	velocity = direction * knockback_force
 	
 	
+	
+func attacking():
+	for overlap in hit_area.get_overlapping_areas():
+		
+		if overlap.is_in_group("hit") and overlap.get_parent().get_parent() != self:
+			overlap.get_parent().get_parent().get_hit(20, global_position)
